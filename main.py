994d7c8f06c20e68ab0cacd750a329c0a4362a1b
jhoanamilenajajoy-flores <fajoyjhoana@gmail.com>
2022-03-11 09:58:09 -0500

    iniciando repositorio

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='172.17.3.7',
+    user='productos',
+    password='productos',
+    database='productos',
+    port=3306
+)
+db.autocommit = True
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    cursor = db.cursor(dictionary=True)
+    
+    cursor.execute('select * from productos')
+    
+    productos = cursor.fetchall()
+    #producto = cursor.fetchone()
+    cursor.close()
+    
+    return render_template("index.html", productos=productos)
+
+@app.get("/crear")
+def crearProducto():
+    return render_template("crear.html")
+
+@app.post("/crear")
+def crearProductoPost():
+    nombre = request.form.get('nombre')
+    price = request.form.get('price')
+    
+    cursor = db.cursor()
+    
+    cursor.execute("insert into productos(nombre, price) values(%s,%s)", (
+        nombre,
+        price,
+    ))
+    
+    cursor.close()
+    return redirect(url_for('index'))
+
+@app.get("/contacto")
+def contacto():
+    return render_template("contactos/index.html")
+
+@app.get("/contacto/<int:contactoId>/edit")
+def editarContacto(contactoId):
+    return render_template('editar.html',id = contactoId)
+
+app.run(debug=True)
